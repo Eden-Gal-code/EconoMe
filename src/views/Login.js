17cdc2fa563acd12c3b30f66c18532193cff1da8
Eden Gal <eden.gal230897@gmail.com>
2020-03-12 14:30:06 +0200

    Online Server Login

--- src/views/Login.js
+++ src/views/Login.js
@@ -29,7 +29,7 @@ class Login extends React.Component {
       password: this.state.Password.slice()
     };
     await axios
-      .post("http://localhost:5000/users/login", LoginInfo)
+      .post("https://econo-me-serv.herokuapp/users/login.com", LoginInfo)
       .then(res => {
         if (res.data !== "0") {
           sessionStorage.setItem("user", JSON.stringify(res.data));
