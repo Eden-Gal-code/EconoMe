4cab2005b1c2e57a894e36fa465ae3e98ce6d952
Eden Gal <eden.gal230897@gmail.com>
2020-03-22 12:24:31 +0200

    Footer fixed

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { MDBContainer, MDBFooter } from "mdbreact";
-import { SocialIcon } from "react-social-icons";
+import Github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+
 const FooterPage = () => {
   return (
     <MDBFooter className="font-small py-3 ">
-      <MDBContainer className="text-center">
-        <SocialIcon url="https://github.com/Eden-Gal-code" />
-        <SocialIcon url="https://www.linkedin.com/in/edengal/" />
+      <MDBContainer>
+        <a href="https://github.com/Eden-Gal-code">
+          <img alt="" src={Github} width={40} height={40}></img>
+        </a>
+        <a href="https://www.linkedin.com/in/edengal/">
+          <img alt="" src={linkedin} width={42} height={42}></img>
+        </a>
       </MDBContainer>
       <div className="footer-copyright text-center ">
         <MDBContainer fluid>Created By Eden Gal</MDBContainer>
