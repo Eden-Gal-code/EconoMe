3ff0cfacbe97c3eacb6b4f7c4ca3c0bf0b5c0216
Eden Gal <eden.gal230897@gmail.com>
2020-03-23 13:59:18 +0200

    bug fixes

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -8,20 +8,6 @@ class Welcome extends React.Component {
     if (sessionStorage.getItem("user") !== null) {
       sessionStorage.removeItem("user");
     }
-    // using Twilio SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    const sgMail = require("@sendgrid/mail");
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: "eden.gal230897@gmail.com",
-      from: "eden.gal230897@gmail.com",
-      subject: "Sending with Twilio SendGrid is Fun",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-    };
-    sgMail.send(msg).then(() => {
-      console.log("email sent");
-    });
   }
   render() {
     return (
